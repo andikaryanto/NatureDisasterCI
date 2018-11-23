90e42a890d8b8f5cb70129a3177df6cfe774e5a4
andikaryanto <41881539+andikaryanto@users.noreply.github.com>
2018-11-23 21:59:06 +0700

    update database.php
    db_debug to false

--- application/config/database.php
+++ application/config/database.php
@@ -107,7 +107,7 @@ else if(ENVIRONMENT === 'development'){
 		'dbdriver' => 'mysqli',
 		'dbprefix' => '',
 		'pconnect' => FALSE,
-		'db_debug' => TRUE,//(ENVIRONMENT !== 'production'),
+		'db_debug' => FALSE,
 		'cache_on' => FALSE,
 		'cachedir' => '',
 		'char_set' => 'utf8',
